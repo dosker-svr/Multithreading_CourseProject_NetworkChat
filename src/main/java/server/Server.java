c18b374855e918f7d494b7fe331abe6eb4f47348
Ilya Kalinin <kalinin.ilya.1922@gmail.com>
2021-08-02 17:47:18 +0300

    Changed 'if (key.isWritable)'

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -6,24 +6,45 @@ import java.nio.ByteBuffer;
 import java.nio.channels.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentHashMap;/*
+ 1 - чем отличается keys от selectedKeys
+     1 - вроде как keys - неизменяемый, selectedKeys - только для удаления
+2 - конкретный key во множестве Selector: это зарегистрированное событие(OP_ACCEPT , OP_READ, OP_WRITE...)
+3 - почему при вводе у клиента новой строки, у неё остаётся хвост от старой? затирается ровно кол-во введенных символов (указатель, position)
+     3 - решено с помощью position + remaining
+4 - Selector - сущность ПРОСЛУШИВАНИЯ каналов. Каналы регистрируют 'событие/состояние' в Selector во множество Set<SelectionKey>.
+ 'событие/состояние' - это SelectionKey(иначе события/эвенты). Т.е. Selector состоит из множества SelectorKey.
+ SelectorKey может быть OP_ACCEPT, OP_CONNECT, OP_READ, OP_WRITE.
+ В SelectorKey - это Канал + Селектор
 
+ ИСПРАВИТЬ поломки:
+ Если один клиент прерывает соединение, сервер выкидывает исключение и выключается. ИСПРАВИТЬ (Удаленный хост принудительно разорвал существующее подключение)
+ Остановлено на:
+ - Создали Общение Клиент-Сервер.
+ - Указания адреса и № порта в настройках в txt или ...
+ - Клиенту перед отправкой сообщения нужно указать своё имя.
+ - Сервер принимает сообщение. Добавляет к нему имя + время. Транслирует всем (или придумать в другое местро)*/
 
 public class Server {
     private static Map<SocketChannel, ByteBuffer> sockets = new ConcurrentHashMap<>(); // мапа всех подключенных каналов
     private static volatile List<String> listUsers = new ArrayList<>();
 
     public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
         try {
             Selector selector = Selector.open(); //
             ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
             serverSocketChannel.bind(new InetSocketAddress(8000));
-            serverSocketChannel.configureBlocking(false);
-            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            serverSocketChannel.configureBlocking(false);// все операции Канала станут неБлокирующими
+// регистрируем селектор за опред.каналом +|+ и операцию, на которой регистрируемся (регистрируемся с Операцией_Соединения):
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);// иначе: Регистрируем событие входящего соединения
             System.out.println("8000");
 
             while (true) {
-                int countChannel = selector.select();
+                int countChannel = selector.select();//этот метод блокирует текущий поток, пока хотя бы один канал не будет готов к событиям, для которых он зарегистрирован
                 if (countChannel == 0) {
                     System.out.println("continue");
                     continue;
@@ -33,7 +54,7 @@ public class Server {
                 while (keysIterator.hasNext()) {
                     SelectionKey key = keysIterator.next();
                     try {
-                        if (key.channel() == serverSocketChannel) {
+                        if (key.isAcceptable()) { //key.channel() == serverSocketChannel
 /*В этом if принимаем соединение от клиента  + регистрируем канал на чтение*/
                             System.out.println("Соединены с Клиентом");
                             SocketChannel clientChannel = serverSocketChannel.accept();
@@ -77,8 +98,19 @@ public class Server {
 
                         } else if (key.isWritable()) {
                             System.out.println("Отправляем сообщения всем Юзерам");
+                            SocketChannel messageChannel = (SocketChannel) key.channel();
+                            ByteBuffer messageBuffer = sockets.get(messageChannel);
+                            messageBuffer.flip();
+                            String messFromClient = new String(messageBuffer.array(),
+                                    messageBuffer.position(),
+                                    messageBuffer.remaining(),
+                                    StandardCharsets.UTF_8);
+                            String messForAllUsers = key.attachment() + " : " + messFromClient;
+                            messageBuffer.clear();
+                            messageChannel.register(selector, SelectionKey.OP_READ, key.attachment());
+
                             for (SocketChannel channel : sockets.keySet()) {
-                                ByteBuffer channelBuffer = sockets.get(channel);
+                                /*ByteBuffer channelBuffer = sockets.get(channel);
                                 channelBuffer.flip();
 
                                 String textFromClient = new String (channelBuffer.array(),
@@ -88,9 +120,9 @@ public class Server {
                                 String textForAllUsers = key.attachment() + " : " + textFromClient;
                                 System.out.println(textForAllUsers);
                                 channelBuffer.clear();
-                                channel.write(ByteBuffer.wrap(textForAllUsers.getBytes(StandardCharsets.UTF_8)));
-
-                                channel.register(selector, SelectionKey.OP_READ, key.attachment());
+                                channel.write(ByteBuffer.wrap(messForAllUsers.getBytes(StandardCharsets.UTF_8)));
+                                channel.register(selector, SelectionKey.OP_READ, key.attachment());*/
+                                channel.write(ByteBuffer.wrap(messForAllUsers.getBytes(StandardCharsets.UTF_8)));
                             }
                         }
                     } finally {
@@ -104,4 +136,16 @@ public class Server {
 
 
     }
+
+    /*private static void acceptConnectAndRegisterChannelToRead(Selector selector, ServerSocketChannel serverSocketChannel) throws IOException {
+        System.out.println("Соединены с Клиентом");
+        SocketChannel clientChannel = serverSocketChannel.accept();
+        clientChannel.configureBlocking(false);
+        sockets.put(clientChannel, ByteBuffer.allocate(2 << 10));
+        clientChannel.register(selector, SelectionKey.OP_READ);
+    }
+
+    private static void sendEveryoneMessage(String message) {
+
+    }*/
 }

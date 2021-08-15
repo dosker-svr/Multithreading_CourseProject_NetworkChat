139cce3dd51070a2cd39128c9224ee5ce2057a1b
Ilya Kalinin <kalinin.ilya.1922@gmail.com>
2021-08-15 13:20:43 +0300

    Server accepts message but doesnt send messages to anyone

--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -10,7 +10,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 
 public class Server {
-    private static Map<SocketChannel, String> session = new HashMap<>(); // мапа для всех сессий
+    private static Map<SocketChannel, String> session = new HashMap<>(); // мапа для всех сессий. значение - имя Юзера
     private static Map<SocketChannel, ByteBuffer> sockets = new ConcurrentHashMap<>(); // мапа всех подключенных каналов
     private static volatile List<String> listUsers = new ArrayList<>();
 
@@ -33,24 +33,21 @@ public class Server {
                 Iterator<SelectionKey> keysIterator = setKeys.iterator();//размер iteartor всегда = 1
                 while (keysIterator.hasNext()) {
                     SelectionKey key = keysIterator.next();
-                    try {
-                        if (key.isAcceptable()) {
+                    keysIterator.remove();
+                    if (key.isAcceptable()) {
 /*В этом if принимаем соединение от клиента  + регистрируем канал на чтение*/
-                            acceptConnection(selector, serverSocketChannel);
+                        acceptConnection(selector, serverSocketChannel);
 
-                        } else if (key.isReadable() && !session.containsKey((SocketChannel) key.channel())) {
+                    } else if (key.isReadable() && !session.containsKey((SocketChannel) key.channel())) {
 /*в этом if читаем Имя Юзера и добавляем в мапу session имя*/
-                            logIn(key, selector);
+                        logIn(key, selector);
 
-                        } else if (key.isReadable() && session.containsKey((SocketChannel) key.channel())) {
+                    } else if (key.isReadable() && session.containsKey((SocketChannel) key.channel())) {
 /*в этом if принимаем сообщение и регистрируем SelectionKey на запись*/
-                            readMessage(key, selector);
-                        } else if (key.isWritable()) {
+                        readMessage(key, selector);
+                    } else if (key.isWritable()) {
 /*в этом if отправляем сообщения всем Клиентам*/
-                            sendEveryoneMessage(selector, key);
-                        }
-                    } finally {
-                        keysIterator.remove();
+                        sendEveryoneMessage(selector, key);
                     }
                 }
             }
@@ -124,7 +121,7 @@ public class Server {
 
             try {
                 socketChannel.register(selector, SelectionKey.OP_WRITE);
-                System.out.println("для клиента="+userName+" - "+fullMessage);
+                System.out.println("для клиента=" + userName + " - " + fullMessage);
                 sockets.get(socketChannel).clear();//NEW
                 socketChannel.write(bufferForMessage);
                 socketChannel.register(selector, SelectionKey.OP_READ);
